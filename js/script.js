da70badb53d06b943296d34c24c9cb6519ed85bf
Arkadiusz Wiśniewski <darek253633@gmail.com>
2023-07-20 03:07:23 +0200

    Add Player Move script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+//Computer Move
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -21,3 +22,18 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+//Player Move
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'Papier';
+} else if (playerInput == '3') {
+  playerMove = 'Nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
