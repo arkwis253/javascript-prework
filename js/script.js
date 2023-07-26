fe36b61c0acdbdb96c227d1d20b85c37647c3793
Arkadiusz Wiśniewski <darek253633@gmail.com>
2023-07-27 01:48:53 +0200

    Code optimization

--- js/script.js
+++ js/script.js
@@ -8,13 +8,8 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-
-
-//
-var argMoveId, argPlayerMove, argComputerMove, computerMove, randomNumber;
-
 /**
- * Describe this function...
+ * funkca zmienia wylosowaną liczbę na nazwę danego ruchu
  */
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
@@ -31,7 +26,8 @@ function getMoveName(argMoveId) {
 }
 
 /**
- * Describe this function...
+ * Funkcja wyświetla wynik rundy w zależności 
+ * od wybranych przez gracza i komputer ruchów.
  */
 function displayResult(argPlayerMove, argComputerMove) {
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
@@ -50,30 +46,34 @@ function displayResult(argPlayerMove, argComputerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 
-
-
-
-//
-var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
-
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
-buttonRock = document.getElementById('button-rock');
-
+/**
+ *pobranie przycisków oraz po kliknięciu w przycisk uruchomienie
+ *funkcji z danym argumentem (papier, kamień, nożyce)
+*/
+const buttonPaper = document.getElementById('button-paper');
+const buttonScissors = document.getElementById('button-scissors');
+const buttonRock = document.getElementById('button-rock');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
 
 /**
- * Describe this function...
+ * Funkcja przyjmuje wartość wciśniętego przycisku,
+ * losuje liczbę z zakresu 1-3, następnie wykonuje 
+ * funkcję getMoveName w celu uzyskania nazwy ruchu
+ * [zamienia 1 na kamień, 2 na papier, 3 na nożyce]
+ * na koniec jest uruchamiana funkcja displayResult,
+ * która jako argumenty przyjmuje ruch gracza oraz komputera
+ * i wyświetla końcowy wynik runy  
  */
 function buttonClicked(argButtonName) {
+    let randomNumber, computerMove;
+    let playerMove = argButtonName;
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
     randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
     computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
-    playerMove = argButtonName;
     displayResult(playerMove, computerMove);
 }
